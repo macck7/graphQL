2c44cc77e1c0e6983465639e94a757834174119b
Mohit <mohitg632@gmail.com>
2024-01-06 10:32:12 +0530

    first commit

---
+++ src/main/java/com/example/graph/grphqlhandson/GrphQlHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.example.graph.grphqlhandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrphQlHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrphQlHandsOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/graph/grphqlhandson/GrphQlHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graph.grphqlhandson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrphQlHandsOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
